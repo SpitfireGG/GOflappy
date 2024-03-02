422f2d65c3bba4711699b655f5a2e6a8ef164bd0
SpitfireGG <raambasnet02@gmail.com>
2024-03-02 11:34:16 +0545

    graphics ..

---
+++ src/graphics/graphics.go
@@ -0,0 +1,11 @@
+package graphics
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+var (
+	BirdUp   = rl.LoadImage("sprites/bluebird-upflap.png")
+	BirdDown = rl.LoadImage("sprites/bluebird-downflap.png")
+	pipeUp   = rl.LoadImage("sprites/pipe-green.png")
+	pipeDown = rl.LoadImage("sprites/pipe-red.png")
+	logo     = rl.LoadImage("sprites/logo.png")
+)
